7afe6aab9bf4ebb7c97198a29a8a2a591df7f96a
Amran Uddin <amran.uddin@sykescottages.co.uk>
2021-02-25 09:17:02 +0000

    pushhhh

---
+++ script.js
@@ -0,0 +1,427 @@
+"use strict";
+
+
+const max_digits = 8;
+const _device_expression = document.getElementById("expression");
+const _device_operand = document.getElementById("operand");
+
+
+const screen = {
+	//arrow functions have been used to define this object's internal methods
+	set: {
+		expression: (value) => { _device_expression.innerText = value; },
+		operand: (value) => { _device_operand.innerText = value; },
+	},
+	clear: {
+		expression: () => { _device_expression.innerText = ""; },
+		operand: () => { _device_operand.innerText = ""; },
+		all: () => { 
+			_device_expression.innerText = "";
+			_device_operand.innerText = "";
+		},
+	},
+	get: {
+		expression: () => { return _device_expression.innerText; },
+		operand: () => { return _device_operand.innerText; },
+	},
+};
+
+
+const calculation = {
+	//use regular functions to define this object's intermal methods
+	_expression: [],
+	push: function (exp) {
+    calculation._expression.push(exp);
+    
+
+   },
+	pop: function () { 
+    calculation._expression.pop();
+    
+  },
+	last: function () {
+    //console.log(calculation.last()); - output “”
+
+    if (calculation._expression.length == 0){   //checks if anything is in array
+      var quote = '';
+      return quote;
+
+    }
+        //if not empty get last value
+    return calculation._expression[calculation._expression.length - 1];
+    },
+	clear: function () {
+    calculation._expression = [];  
+    calculation._expression.length = 0; 
+   },
+	debug: function () { 
+    console.log(calculation._expression)
+  },
+
+	expression: function() {
+    return calculation._expression.join(' ');
+  }
+};
+
+
+function append_value(original, append, glue, spacer=false) {
+  var space = "";
+  if (spacer == true){
+    var space = " ";
+  }
+  if (original == ""){
+    console.log(append);
+    return append;
+
+  }
+  else if (append == ""){
+    console.log(original);
+    return original;
+
+  }
+  else if (glue == ""){
+    console.log(original + space + append);
+    return original + space + append;
+
+  }
+  else {
+
+    console.log(original + glue + append);
+    return original + space + glue + space +append;
+  }
+    
+}
+
+
+function valid_leadingzeros(value) {
+
+  if ((value[0] == '0') && (value[1] == '0')){
+    console.log('false');
+    return false;
+  }
+  else {
+    console.log('true');
+    return true;
+  }
+}
+
+// uncertainty is '.' true or false is '.1' true or false
+function valid_decimals(value) {
+  var count = value.split('.').length - 1;  //splits the string into an array by .
+  if (count > 1) {                  //number of . = number of elements in array -1
+    console.log('false');   //e.g '123.234' = ['123','234] 1 dot = 2 array elements
+    return false;            // '24222342' = ['2422342'] 0 dot = 1 array elements
+  }
+  else {
+    console.log('true');
+    return true;
+  }
+}
+
+
+function trim_invalid_numerics(value) {
+  var numeric = "";
+  var isNumeric= ['0','1','2','3','4','5','6','7','8','9','.','-'];
+
+  for (var i = 0; i < value.length; i++) {   
+    if (isNumeric.includes(value[i])) {   //checks if each digit is numeric or . -
+      numeric = numeric + value[i];   
+    }
+  
+  }
+  
+  return numeric 
+}
+
+
+function control_pressed(control) {
+	switch(control) {
+		case "c":
+    screen.clear.operand()
+    console.log("c");
+    break
+		case "ac": 
+    screen.clear.operand();
+    screen.clear.expression();
+    calculation.clear();
+    console.log("ac");
+    break
+		case "=": 
+    var total = screen.get.operand();
+    calculation.push(total)
+    var express = calculation.expression();
+    screen.set.expression(express)
+    var val = evaluate(express)
+    console.log("=");
+    screen.set.operand(val)
+    calculation.clear()
+    break
+	}
+}
+
+
+function digit_pressed(digit) {
+	console.log("digit pressed: " + digit);
+  var total = screen.get.operand();
+  if (total.length == max_digits){
+    return
+  }
+  total = total + digit
+  //total = leading_zero(total)
+
+  
+  screen.set.operand(total);
+  
+
+}
+
+
+function operator_pressed(operator) {
+	console.log("operator pressed: " + operator);
+  //checks if last expression was an operator
+  var oper = ['+','-','/','*']
+  var total = screen.get.operand();
+  if (oper.includes(calculation.last()) && total ==''){
+    console.log("already pressed an operator")
+    calculation.pop()
+  }
+  
+  total = leading_zero(total)
+  calculation.push(total)
+  calculation.push(operator);
+  var express = calculation.expression()
+  screen.set.expression(express)
+  screen.clear.operand()
+
+
+}
+function trailing_zero(exp){
+  var str = exp.toString()
+  //console.log(typeof(str))
+  
+  while (str[str.length -1] == '0'){
+    str = str.slice(0,str.length - 1)
+    
+  }
+  return str
+  
+}
+function leading_zero(expression){
+  
+  while ((expression[0] == '0') && (expression[1] != '.') && (expression.length > 1)){
+    expression= expression.slice(1,expression.length);
+    
+  }
+  return expression;
+  
+}
+function leading_zero_array(expression){
+  
+  var newarr = []
+  for(let i = 0; i < expression.length; i++) {
+    newarr.push(leading_zero(expression[i]))
+
+  }
+  return newarr
+
+}
+function evaluate(expression) {
+  console.log('computing')
+  var expression = expression.replace(/\s/g, '');
+  //get rid of zero at every stage
+  if (expression == ''){
+    return ""
+  }
+
+  
+  if (expression.replace((/[+*\/]/g),"") != trim_invalid_numerics(expression)){
+    console.log(expression.replace((/[+*\/]/g),"") + " -- " + trim_invalid_numerics(expression))
+    return "ERROR"
+  }
+  
+  expression = expression.split('/')  
+  expression = leading_zero_array(expression); 
+  expression = expression.join('/')
+  expression = expression.split('*')
+  expression = leading_zero_array(expression); 
+  expression = expression.join('*') 
+  expression = expression.split('-')
+  expression = leading_zero_array(expression); 
+  expression = expression.join('-')
+  expression = expression.split('+')
+  expression = leading_zero_array(expression); 
+  expression = expression.join('+')
+  
+  
+  console.log('expression')
+  console.log(expression);
+  var arr = []
+  // cant eval 005*02 need to get rid of zero
+  var answer = eval(expression).toFixed(11);
+  answer = trailing_zero(answer);
+  //screen.set.operand(answer)
+  console.log('answer');
+  console.log(answer);
+  
+  ;
+  var arr = expression.match(/[+*\/-]/g);
+  answer = parseFloat(answer);
+  if (arr == null){
+    console.log('No Operator');
+
+    return answer
+  }
+  
+  var operIndex = getAllIndexes(arr,expression);
+  var indexOp = getSecond(operIndex);
+  //console.log(arr)
+  //console.log(indexOp)
+  
+  return answer;
+
+  /*
+  
+  TRIED TO DO IT MYSELF
+  arr = expression.match(/[+*-\/]/g);
+  console.log(arr);
+  //getAllIndexes(arr,expression);
+  var operIndex = getAllIndexes(arr,expression);
+  var locationsOfadd = getIndex(operIndex,'+');
+  var indexOp = getSecond(operIndex);
+  //console.log(indexOp);
+  //console.log(locationsOfadd);
+  //console.log(operIndex);
+
+  
+  var locationsOfdiv = getIndex(operIndex,'/');
+  
+  var locationsOfmult = getIndex(operIndex,'*');
+  var locationsOfminus = getIndex(operIndex,'-');
+  var locationsOfadd = getIndex(operIndex,'+');
+  
+  if (locationsOfdiv.length > 0){
+    console.log(locationsOfdiv);
+    var index = locationsOfdiv[0];
+    console.log(index)
+    var indexofOperator = indexOp.indexOf(index);
+    
+    
+    var operatorBeforeIndex = indexOp[indexofOperator -1];
+    console.log(operatorBeforeIndex);
+    var operatorAfterIndex = indexOp[indexofOperator +1];
+    var charBetween = charRange(operatorBeforeIndex + 1,operatorAfterIndex,expression);
+    console.log(operatorAfterIndex);
+    var divise = charBetween.split('/');
+    result = parseInt(divise[0])/parseInt(divise[1])
+    console.log(result + "woow")
+
+  }
+  */
+
+}
+function maxLength(number,maxDigit=max_digits){
+  number = number.toString();
+  number = number.split('.');
+  
+  if (number[0].length > max_digits){
+    var num = parseInt(number[0][8])
+    var add = 0;
+    if (num >='5'){
+      add = 1
+    }
+    var rounded = eval(number[0].slice(0,8) + '+' + add.toString())
+    var power = number[0].length - 1
+    rounded = rounded.toString()
+    var numberE = rounded[0] + "." + rounded.slice(1,8) + "E+" + power
+    return numberE
+  }
+  if ((number[1].length > max_digits) && (number[0]=='0' || number[0]=='')){
+    
+    var num = parseInt(number[1][8])
+    var add = 0;
+    if (num >='5'){
+      add = 1
+    }
+    var rounded = eval(number[1].slice(0,8) + '+' + add.toString())
+    var power = number[1].length - 1
+    rounded = rounded.toString()
+    var numberE = rounded[0] + "." + rounded.slice(1,8) + "E-" + power
+    return numberE
+  }
+  else if (number[0].length + number[1].length > 8) {
+    return (number.join('.')).slice(0,9)
+  }
+}
+function consecutive(arr){
+  for(let i = 0; i < arr.length - 1; i++){
+    if (arr[i] == arr[i+1] - 1){
+      return i;
+    }
+  }
+}
+
+function getAllIndexes(arr, str) {
+  
+  var newarr = [];
+  var strl = str.length - 1;
+  var j = 0;
+  //return newarr
+  for(let i = 0; i < arr.length; i++){
+    
+    var index = str.indexOf(arr[i],j)
+    newarr.push([arr[i],index]);  
+    j = parseInt(index) + 1;
+    
+
+  }
+  return newarr;
+
+  
+    
+
+}
+function getIndex(arr,oper){
+  var newarr = [];
+    for(let i = 0; i < arr.length; i++){
+    
+      if (arr[i][0] == oper){
+        newarr.push(arr[i][1]);
+      }
+  }
+  return newarr
+}
+function getSecond(arr){
+  var newarr = [];
+  for(let i = 0; i < arr.length; i++){
+    newarr.push(arr[i][1])
+  }
+  return newarr
+
+}
+function charRange(x,y,str){
+  var newstr = "";
+  for(let i = x; i < y; i++) {
+    newstr += str[i];
+  }
+ return newstr
+}
+//search for all HTML objects that are using the class name 'button'
+var buttons = document.getElementsByClassName('button');
+for(let i = 0; i < buttons.length; i++) { //loop through each 'button' instance
+	buttons[i].addEventListener('click', function() { //attach a 'click' event listener
+    	switch(this.dataset.action) { //invoke a specific function based on the type of button 'clicked'
+			//pass the ID to the selected function
+			case("digit"): digit_pressed(this.id); break; 
+			case("operator"): operator_pressed(this.id); break;
+			case("control"): control_pressed(this.id); break;
+		}
+	})
+}
+
+//once the oload event has fired, execute any requested functions
+window.onload = () => {
+  screen.clear.all();
+  
+  
+};
+
